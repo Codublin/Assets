a8e78ee6aca627300559a75b15840f546782bf0e
Thomasina Egan <d13126034@ast-1027-m19.ict.ad.dit.ie>
2014-03-10 18:42:16 +0000

    monday 10th march D13126034
    exam closed book

---
+++ chaser.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class chaser : MonoBehaviour {
+
+	
+	
+	
+Vector3 = force+=Vector3.zero
+Vector3 = velocity += Vector3.zero
+			
+			
+force maxSpeed = velcoity = 20.0f
+force mass = 1.0f		
+		
+float velocity/mass*Time.deltaTime;
+float Speed*Time.deltaTime;
+	
+	
+	
+	void Update () {
+		
+Vector3 = newVector advise(enemy)
+ time.diastance-position*Time.deltaTime;
+		
+	
+		
+	}
+}
---
+++ enemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+	
+	
+	
+Vector3 = force +=  Vector3.zero
+Vector3 = velocity += Vector3.zero
+			
+			
+force maxSpeed = velcoity =20.0f
+force mass = 1.0f		
+		
+float velocity/mass*Time.deltaTime;
+float Speed*Time.deltaTime;
+	
+	
+	
+	void Update () {
+	
+		
+	}
+}
